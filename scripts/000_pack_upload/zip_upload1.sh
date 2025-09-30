1d433f1f17ce7636dfe4cda36b810fdfdec21148
NOVAglow646 <190001925@pku.edu.cn>
2025-10-01 00:12:28 +0800

    10.1

---
+++ scripts/000_pack_upload/zip_upload1.sh
@@ -0,0 +1,37 @@
+ckpt_name=9.30_avt_v3_wo_maze_latent8_ce4.0_align-wt0.0001_emph-wt1.0
+step=200
+src=/mmu_vcg_ssd/shiyang06/Project/Latent_Think/checkpoint/avt_v3/${ckpt_name}/checkpoint-${step}
+output=/mmu_vcg_ssd/shiyang06/Project/Latent_Think/checkpoint/avt_v3/${ckpt_name}-checkpoint-${step}.zip
+include_subdir=False
+
+if [ -z "$src" ] || [ -z "$output" ] || [ -z "$include_subdir" ]; then
+    echo "用法: $0 <source_folder> <output_zip> <include_subdir:True|False>"
+    exit 1
+fi
+
+if [ "$include_subdir" = "True" ]; then
+    # 压缩整个目录（包含子目录），只保留最后一级目录名
+    (cd "$(dirname "$src")" && zip -r "$output" "$(basename "$src")")
+else
+    # 只压缩目录下的文件，不包含子目录，保留最后一级目录名
+    (cd "$(dirname "$src")" && zip "$output" "$(basename "$src")"/*)
+fi
+
+python /mmu_vcg_ssd/shiyang06/Tool/huggingface.py --item /mmu_vcg_ssd/shiyang06/Project/Latent_Think/checkpoint/avt_v3/${ckpt_name}-checkpoint-${step}.zip
+
+
+step=400
+if [ -z "$src" ] || [ -z "$output" ] || [ -z "$include_subdir" ]; then
+    echo "用法: $0 <source_folder> <output_zip> <include_subdir:True|False>"
+    exit 1
+fi
+
+if [ "$include_subdir" = "True" ]; then
+    # 压缩整个目录（包含子目录），只保留最后一级目录名
+    (cd "$(dirname "$src")" && zip -r "$output" "$(basename "$src")")
+else
+    # 只压缩目录下的文件，不包含子目录，保留最后一级目录名
+    (cd "$(dirname "$src")" && zip "$output" "$(basename "$src")"/*)
+fi
+
+python /mmu_vcg_ssd/shiyang06/Tool/huggingface.py --item /mmu_vcg_ssd/shiyang06/Project/Latent_Think/checkpoint/avt_v3/${ckpt_name}-checkpoint-${step}.zip
--- sft_eval_obs_acc.sh
+++ sft_eval_obs_acc.sh
@@ -3,7 +3,7 @@ export PATH=$CUDA_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$CUDA_HOME/lib64:${LD_LIBRARY_PATH:-}
 conda activate mirage
 cd /home/dids/shiyang/codes/abstract-visual-token
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=0
 export TOKENIZERS_PARALLELISM=false
 python -m src.sft_eval_obs_acc_non_dist \
     --bsz 1 \
@@ -13,77 +13,8 @@ python -m src.sft_eval_obs_acc_non_dist \
     "./new/created_dataset/filtered_data/Zebra_CoT_visual_search/filtered_train_w_metadata_9.24_further_washed.json" \
   "./new/created_dataset/filtered_data/CoM_w_MathVista/filtered_train_w_metadata_9.1.json" \
   "./new/created_dataset/filtered_data/ReFocus/filtered_train_w_metadata_9.1.json" \
+  "./new/created_dataset/filtered_data/Zebra_CoT_count/filtered_train_w_metadata_9.25_max_seq_len4096_max_seq_len3000.json" \
     --log_file "./log.txt" \
-    --load_model_path /home/dids/shiyang/checkpoints/avt_sft/9.24_debug_avt_sft_com_refocus_search-fwsh_ce5.0/checkpoint-100
+    --load_model_path /home/dids/shiyang/checkpoints/after9.28/9.27_avt_sft_wo_maze_ce2.0.zip/9.27_avt_sft_wo_maze_ce2.0
 
 
-export CUDA_HOME=/usr/local/cuda-12.6
-export PATH=$CUDA_HOME/bin:$PATH
-export LD_LIBRARY_PATH=$CUDA_HOME/lib64:${LD_LIBRARY_PATH:-}
-conda activate mirage
-cd /home/dids/shiyang/codes/abstract-visual-token
-export CUDA_VISIBLE_DEVICES=3
-export TOKENIZERS_PARALLELISM=false
-python -m src.sft_eval_obs_acc_non_dist \
-    --bsz 1 \
-    --task "mm-reasoning" \
-    --deepspeed ./deepspeed/ds_zero2_gpu.json \
-    --data_path \
-    "./new/created_dataset/filtered_data/Zebra_CoT_visual_search/filtered_train_w_metadata_9.24_further_washed.json" \
-  "./new/created_dataset/filtered_data/CoM_w_MathVista/filtered_train_w_metadata_9.1.json" \
-  "./new/created_dataset/filtered_data/ReFocus/filtered_train_w_metadata_9.1.json" \
-    --log_file "./log.txt" \
-    --load_model_path /home/dids/shiyang/checkpoints/avt_sft/9.24_debug_avt_sft_com_refocus_search-fwsh_ce5.0/checkpoint-180
-
-export CUDA_HOME=/usr/local/cuda-12.6
-export PATH=$CUDA_HOME/bin:$PATH
-export LD_LIBRARY_PATH=$CUDA_HOME/lib64:${LD_LIBRARY_PATH:-}
-conda activate mirage
-cd /home/dids/shiyang/codes/abstract-visual-token
-export CUDA_VISIBLE_DEVICES=2
-export TOKENIZERS_PARALLELISM=false
-python -m src.sft_eval_obs_acc_non_dist \
-    --bsz 1 \
-    --task "mm-reasoning" \
-    --deepspeed ./deepspeed/ds_zero2_gpu.json \
-    --data_path \
-  "./new/created_dataset/filtered_data/CoM_w_MathVista/filtered_train_w_metadata_9.1.json" \
-  "./new/created_dataset/filtered_data/ReFocus/filtered_train_w_metadata_9.1.json" \
-    --log_file "./log.txt" \
-    --load_model_path /home/dids/shiyang/checkpoints/avt_sft/9.22_debug_avt_sft_com_refocus_search_ce5.0/checkpoint-1000
-
-export CUDA_HOME=/usr/local/cuda-12.6
-export PATH=$CUDA_HOME/bin:$PATH
-export LD_LIBRARY_PATH=$CUDA_HOME/lib64:${LD_LIBRARY_PATH:-}
-conda activate mirage
-cd /home/dids/shiyang/codes/abstract-visual-token
-export CUDA_VISIBLE_DEVICES=3
-export TOKENIZERS_PARALLELISM=false
-python -m src.sft_eval_obs_acc_non_dist \
-    --bsz 1 \
-    --task "mm-reasoning" \
-    --deepspeed ./deepspeed/ds_zero2_gpu.json \
-    --data_path \
-  "./new/created_dataset/filtered_data/CoM_w_MathVista/filtered_train_w_metadata_9.1.json" \
-  "./new/created_dataset/filtered_data/ReFocus/filtered_train_w_metadata_9.1.json" \
-    --log_file "./log.txt" \
-    --load_model_path /home/dids/shiyang/checkpoints/avt_sft/9.22_debug_avt_sft_com_refocus_search_ce5.0/checkpoint-1100
-
-
-export CUDA_HOME=/usr/local/cuda-12.6
-export PATH=$CUDA_HOME/bin:$PATH
-export LD_LIBRARY_PATH=$CUDA_HOME/lib64:${LD_LIBRARY_PATH:-}
-conda activate mirage
-cd /home/dids/shiyang/codes/abstract-visual-token
-export CUDA_VISIBLE_DEVICES=3
-export TOKENIZERS_PARALLELISM=false
-python -m src.sft_eval_obs_acc_non_dist \
-    --bsz 1 \
-    --task "mm-reasoning" \
-    --deepspeed ./deepspeed/ds_zero2_gpu.json \
-    --data_path \
-  "./new/created_dataset/filtered_data/CoM_w_MathVista/filtered_train_w_metadata_9.1.json" \
-  "./new/created_dataset/filtered_data/ReFocus/filtered_train_w_metadata_9.1.json" \
-    --log_file "./log.txt" \
-    --load_model_path /home/dids/shiyang/checkpoints/avt_sft/9.22_debug_avt_sft_ce5.0/checkpoint-90
-  
