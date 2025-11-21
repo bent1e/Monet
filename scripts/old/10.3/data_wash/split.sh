e37a511041cd0a5a3754b55acbfe06183da824f0
DogNeverSleep <frankyang151@163.com>
2025-11-21 10:53:40 +0800

    init

--- scripts/old/10.3/data_wash/split.sh
+++
@@ -1,3 +0,0 @@
-conda activate mirage
-cd /mmu_vcg_ssd/shiyang06/Project/Latent_Think/abstract-visual-token/
-python ./new/dataset_utils/split_json.py  /ytech_m2v5_hdd/workspace/kling_mm/shiyang06/Dataset/abstract_visual/Visual-CoT/viscot_363k.json 6
